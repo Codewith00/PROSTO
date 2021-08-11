902f014b0ce8809c61f46d1808701d8584284a3f
Anton Markov <master0jide@gmail.com>
2021-08-11 23:55:32 +1000

    Homework fixed

--- 03_Variables_types/src/main/kotlin/Main.kt
+++ 03_Variables_types/src/main/kotlin/Main.kt
@@ -1,15 +1,12 @@
 fun main(args: Array<String>) {
 
 
-    var firstName: String = "Anton"
-    var lastName: String = "Markov"
-    var height: Double = 149.9
-    var weight: Float = 95.2f
+    val firstName: String = "Anton"
+    val lastName: String = "Markov"
+    var height: Double = 169.9
+    var weight: Float = 45.2f
     var isChild: Boolean = height < 150 || weight < 40
-
-    var infoChild: String = "Adult"
-    if(isChild == true)
-        infoChild = "Child"
+    var infoChild = if(isChild) "Child" else "Adult"
     var info: String = """firstName - $firstName
         |lastName - $lastName
         |height - $height
@@ -17,8 +14,18 @@ fun main(args: Array<String>) {
         |is Child? - $infoChild
     """.trimMargin()
     println(info)
+    height = 120.3
+    weight = 35.1f
+    isChild = height < 150 || weight < 40
+    infoChild = if(isChild) "Child" else "Adult"
+    info ="""
+        | 
+        |firstName - $firstName
+        |lastName - $lastName
+        |height - $height
+        |weight - $weight
+        |is Child? - $infoChild
+    """.trimMargin()
+    println(info)
 
-
-    // Try adding program arguments at Run/Debug configuration
-   // println("Program arguments: ${args.joinToString()}")
 }
